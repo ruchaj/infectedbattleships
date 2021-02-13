043b0f68172145d7a4bae66fd1589d41ec563ee2
ruchaj <ruchajrocks@gmail.com>
2021-02-13 12:40:20 -0800

    Add existing file

--- battleship/src/DragNDrop.js
+++ battleship/src/DragNDrop.js
@@ -5,3 +5,23 @@ import AppDragDropDemo from './AppDragDropDemo';
 ReactDOM.render(<AppDragDropDemo />,     document.getElementById("root"));
 import React, { Component } from 'react';
 export default class AppDragDropDemo extends Component {  render () {    return (      <div className="container-drag">        DRAG & DROP DEMO      </div>    );  }}
+export default class AppDragDropDemo extends Component {      
+    state = {            
+        tasks: [{name:"Learn Angular",             
+        category:"wip",              
+        bgcolor: "yellow"},                        
+        {name:"React",              
+        category:"wip",              
+        bgcolor:"pink"},                        
+        {name:"Vue",              
+        category:"complete",              
+        bgcolor:"skyblue"}                
+    ]}
+    render () {    
+        return (      
+        <div className="container-drag">        
+        DRAG & DROP DEMO      </div>    
+        );  
+    }
+}
+
