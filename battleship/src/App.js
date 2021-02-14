ce28d8e26f499cf023c121d5897f881945a0b311
ruchaj <ruchajrocks@gmail.com>
2021-02-13 17:23:31 -0800

    Button attempt

--- battleship/src/App.js
+++ battleship/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 import Board from './Board';
-
+import EndButton from './EndButton';
 function App() {
   return (
     <div className="App">
       <Board nrows={10} ncols={10} />
       <Board nrows={10} ncols={10} />
-
+      <EndButton b/>
     </div>
   );
 }
