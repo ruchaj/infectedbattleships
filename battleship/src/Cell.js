32e3d90b601b72f660533cb2f16d0c2f71d7a805
Nortus222 <nortus222@gmail.com>
2021-02-13 19:30:40 -0800

    added check state implementation

--- battleship/src/Cell.js
+++ battleship/src/Cell.js
@@ -6,6 +6,7 @@ class Cell extends React.Component {
     constructor(props) {
         super(props);
         this.onClick = this.onClick.bind(this);
+        this.checkState = this.checkState.bind(this);
         
         this.state = {
             didBombed: false,
@@ -16,13 +17,33 @@ class Cell extends React.Component {
 
     onClick = () => {
         if (this.state.user === 2) {
-            this.setState({didBombed: !this.state.didBombed});
+            this.setState({didBombed: true});
         }
+    }
+
+    checkState = () => {
+        let str;
+        if (this.state.user === 1) {
+            if(this.state.didBombed) {
+                str = "square-user-bombed";
+            } else {
+                console.log(1);
+                str = "square";
+            }
+        } else if (this.state.user === 2) {
+            if (this.state.didBombed) {
+                str = "square-comp-person fadeIn";
+            } else {
+                str = "square";
+            }
+        }
+
+        return str;
     }
 
     render() {
         return(
-            <button className={(this.state.didBombed) ? "square-b" : "square"} onClick={this.onClick}></button>
+            <button className={this.checkState()} onClick={this.onClick}></button>
         );
     }
 
